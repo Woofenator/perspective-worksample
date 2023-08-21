ab682c3277a8e4ca6725fc75b2dd6bcdbb7b94fa
Maris Jirgens <maris@saltox.co>
2023-08-21 11:48:20 +0300

    feat: set-up orm with express

--- src/database/orm.ts
+++ src/database/orm.ts
@@ -1,9 +1,11 @@
 import { MikroORM } from '@mikro-orm/core';
 import { BetterSqliteDriver } from '@mikro-orm/better-sqlite';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 
 export const orm = await MikroORM.init<BetterSqliteDriver>({
-    entities: ['./dist/database/entities'],
-    entitiesTs: ['./src/database/entities'],
+    entities: ['./dist/database/entities/*.js'],
+    entitiesTs: ['./src/database/entities/*.ts'],
+    metadataProvider: TsMorphMetadataProvider,
     dbName: 'work-example',
     type: 'better-sqlite',
 });
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import { RequestContext } from '@mikro-orm/core';
+import { orm } from './database/orm';
 
 dotenv.config();
 
 const app: Express = express();
-app.use(cors())
-  .use(express.json())
-  .options('*', cors());
+app.use(cors()).use(express.json()).options('*', cors());
+app.use((_req, _res, next) => {
+    RequestContext.create(orm.em, next);
+});
 
 app.post('/users', (req: Request, res: Response) => {
-  res.send({}).status(201);
+    res.send({}).status(201);
 });
 app.get('/users', (req: Request, res: Response) => {
-  res.send([]).status(200);
+    res.send([]).status(200);
 });
 
 const port = process.env.PORT || 3111;
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
