d1ebb7603b223f38f648f38c1bdf4f3d07be2071
Maris Jirgens <maris@saltox.co>
2023-08-23 13:00:00 +0300

    feat: add user get route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Router } from 'express';
 import asyncHandler from 'express-async-handler';
-import { body, validationResult } from 'express-validator';
+import { body, validationResult, query } from 'express-validator';
 import httpStatus from 'http-status';
 import { User } from '../database/entities/user.js';
 import { orm } from '../database/orm.js';
@@ -8,21 +8,46 @@ import { HttpUnprocessableEntityException } from '../exceptions/HttpUnprocessabl
 
 const router = Router();
 
-router.route('').post(
-    body('name').trim().notEmpty().isString(),
-    body('email').isEmail().trim(),
-    asyncHandler(async (req: Request, res: Response) => {
-        const validation = validationResult(req);
+router
+    .route('')
+    .post(
+        body('name').trim().notEmpty().isString(),
+        body('email').isEmail().trim(),
+        asyncHandler(async (req: Request, res: Response) => {
+            const validation = validationResult(req);
 
-        if (!validation.isEmpty()) {
-            throw new HttpUnprocessableEntityException(validation.array());
-        }
+            if (!validation.isEmpty()) {
+                throw new HttpUnprocessableEntityException(validation.array());
+            }
 
-        const user = new User(req.body);
-        await orm.em.persistAndFlush([user]);
+            const user = new User(req.body);
+            // Save user entity in database
+            await orm.em.persistAndFlush([user]);
 
-        res.status(httpStatus.CREATED).send({ status: 'success' });
-    }),
-);
+            res.status(httpStatus.CREATED).send({ status: 'success' });
+        }),
+    )
+    .get(
+        query('created').optional().toLowerCase().isIn(['asc', 'desc']),
+        asyncHandler(async (req: Request, res: Response) => {
+            const validation = validationResult(req);
+
+            if (!validation.isEmpty()) {
+                throw new HttpUnprocessableEntityException(validation.array());
+            }
+
+            const users = await orm.em.find(
+                User,
+                {},
+                {
+                    // Inline check for `created` field in query.
+                    // Not exactly readable, but best I could come up with to not overcomplicate things
+                    orderBy: req.query.created ? { createdAt: req.query.created } : {},
+                },
+            );
+
+            res.status(httpStatus.OK).send(users);
+        }),
+    );
 
 export { router as userRouter };
